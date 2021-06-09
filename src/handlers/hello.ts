d355b65a50985cc6d7bfe1d271f707efb4ccba2b
JBlack96 <67678754+JBlack96@users.noreply.github.com>
2021-06-09 18:46:42 +0100

    reformatted project

--- src/handlers/hello.ts
+++ src/handlers/hello.ts
@@ -1,4 +1,4 @@
-export const hello = async (event: any = {}): Promise<any> => {
+export const handler = async (event: any = {}): Promise<any> => {
   console.log("Hello World");
   return { statusCode: 200, body: "Hello world" };
 };
